c2614927bcfa6df636457d2c93092482be119543
姚一鸣 <yaoyiming123@live.com>
2023-11-25 19:41:27 +0800

    第一次提交

---
+++ PowerShortcut.Core/SettingsService.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Windows.Storage;
+
+namespace PowerShortcut.Core
+{
+    public class SettingsService : ObservableObject
+    {
+        private const string SETTING_NAME_APPEARANCEINDEX = "AppearanceIndex";
+        private const string SETTING_NAME_BACKDROPINDEX = "BackdropIndex";
+
+        private ApplicationDataContainer _localSettings = ApplicationData.Current.LocalSettings;
+
+        public Action<int> OnAppearanceSettingChanged { get; set; } = null;
+        public Action<int> OnBackdropSettingChanged { get; set; } = null;
+
+        // 设置的应用程序的主题 0-System 1-Dark 2-Light
+        private int _appearanceIndex = -1;
+        public int AppearanceIndex
+        {
+            get
+            {
+                try
+                {
+                    if (_appearanceIndex < 0)
+                    {
+                        if (_localSettings.Values[SETTING_NAME_APPEARANCEINDEX] == null)
+                        {
+                            _appearanceIndex = 0;
+                        }
+                        else if (_localSettings.Values[SETTING_NAME_APPEARANCEINDEX]?.ToString() == "0")
+                        {
+                            _appearanceIndex = 0;
+                        }
+                        else if (_localSettings.Values[SETTING_NAME_APPEARANCEINDEX]?.ToString() == "1")
+                        {
+                            _appearanceIndex = 1;
+                        }
+                        else if (_localSettings.Values[SETTING_NAME_APPEARANCEINDEX]?.ToString() == "2")
+                        {
+                            _appearanceIndex = 2;
+                        }
+                        else
+                        {
+                            _appearanceIndex = 0;
+                        }
+                    }
+                }
+                catch { }
+                if (_appearanceIndex < 0) _appearanceIndex = 0;
+                return _appearanceIndex < 0 ? 0 : _appearanceIndex;
+            }
+            set
+            {
+                SetProperty(ref _appearanceIndex, value);
+                ApplicationData.Current.LocalSettings.Values[SETTING_NAME_APPEARANCEINDEX] = _appearanceIndex;
+                OnAppearanceSettingChanged?.Invoke(_appearanceIndex);
+            }
+        }
+
+        // 设置的应用程序的背景材质 0-Mica 1-Acrylic
+        private int _backdropIndex = -1;
+        public int BackdropIndex
+        {
+            get
+            {
+                try
+                {
+                    if (_backdropIndex < 0)
+                    {
+                        if (_localSettings.Values[SETTING_NAME_BACKDROPINDEX] == null)
+                        {
+                            _backdropIndex = 0;
+                        }
+                        else if (_localSettings.Values[SETTING_NAME_BACKDROPINDEX]?.ToString() == "0")
+                        {
+                            _backdropIndex = 0;
+                        }
+                        else if (_localSettings.Values[SETTING_NAME_BACKDROPINDEX]?.ToString() == "1")
+                        {
+                            _backdropIndex = 1;
+                        }
+                        else
+                        {
+                            _backdropIndex = 0;
+                        }
+                    }
+                }
+                catch { }
+                if (_backdropIndex < 0) _backdropIndex = 0;
+                return _backdropIndex < 0 ? 0 : _backdropIndex;
+            }
+            set
+            {
+                SetProperty(ref _backdropIndex, value);
+                ApplicationData.Current.LocalSettings.Values[SETTING_NAME_BACKDROPINDEX] = _backdropIndex;
+                OnBackdropSettingChanged?.Invoke(_backdropIndex);
+            }
+        }
+    }
+
+}
---
+++ PowerShortcut/ViewModels/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using PowerShortcut.Core;
+using PowerShortcut.Models;
+
+namespace PowerShortcut.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        private static Lazy<MainViewModel> _lazyVM = new Lazy<MainViewModel>(() => new MainViewModel());
+        public static MainViewModel Instance => _lazyVM.Value;
+
+        public SettingsService AppSettings { get; set; } = new SettingsService();
+
+        /// <summary>
+        /// 控制主窗口根据当前的主题进行切换
+        /// </summary>
+        public Action ActSwitchAppTheme { get; set; } = null;
+
+        /// <summary>
+        /// 控制主窗口根据当前的设置更改背景材质
+        /// </summary>
+        public Action ActChangeBackdrop { get; set; } = null;
+
+        public ObservableCollection<MainNavigationBase> MainNavigationItems = new ObservableCollection<MainNavigationBase>();
+
+        public ObservableCollection<MainNavigationBase> MainNavigationFooterItems = new ObservableCollection<MainNavigationBase>();
+
+        public MainViewModel()
+        {
+            AppSettings.OnAppearanceSettingChanged += (index) => { ActSwitchAppTheme?.Invoke(); };
+            AppSettings.OnBackdropSettingChanged += (index) => { ActChangeBackdrop?.Invoke(); };
+
+            MainNavigationItems.Add(new MainNavigationHeader("Power Shortcut"));
+            MainNavigationItems.Add(new MainNavigationItem("所有脚本", "all", "\uE74C"));
+            MainNavigationItems.Add(new MainNavigationItem("添加", "add", "\uE109"));
+            MainNavigationItems.Add(new MainNavigationHeader("分类"));
+            MainNavigationItems.Add(new MainNavigationItem("PowerShell", "ps", "\uE62F"));
+            MainNavigationItems.Add(new MainNavigationItem("批处理", "bat", "\uE756"));
+
+            MainNavigationFooterItems.Add(new MainNavigationSeparator());
+            MainNavigationFooterItems.Add(new MainNavigationSettingItem());
+
+        }
+
+    }
+}
